72ebde343b4f990d41a1039b4609af1f349ef63a
Godfred-A99 <godfredadjetey99@gmail.com>
2023-10-23 02:55:16 -0700

    added a function to delete from the db

--- details.php
+++ details.php
@@ -2,6 +2,20 @@
 
     include('config/db_connect.php');
 
+        if(isset($_GET['delete'])) {
+            $id_to_delete = mysqli_real_escape_string($conn, $_GET['id_to_delete']);
+
+            $sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
+
+            if(mysqli_query($conn, $sql)){
+                //success
+                header('Location: index.php ');
+            }  else {
+                //failure
+                echo 'query error: ' . mysqli_error($conn);
+            }
+        }
+
     // check GET request id param
     if(isset($_GET['id'])) {
 
@@ -24,20 +38,28 @@
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
     <?php include('templetes/header.php');?>
 
         <div class="container center">
             <?php if($pizza): ?>
+
                 <h4><?php echo htmlspecialchars($pizza['title']);  ?></h4>
                 <p>Created by: <?php echo htmlspecialchars($pizza['email']);  ?> </p>
                 <p><?php echo htmlspecialchars($pizza['created_at']);  ?></p>
                 <h5>Ingredients: </h5>
                 <p><?php echo htmlspecialchars($pizza['ingredient']);  ?></p>
+
+            <!--Delete Form-->
+                <form action="details.php" method= "GET">
+                    <input type = "hidden" name = "id_to_delete" value = "<?php echo $pizza['id'] ?>">
+                    <input type="submit" name="delete" value = "Delete" class="btn brand z-depth-0">
+                </form>
+
             <?php else: ?> 
                 <h5>NO SUCH PIZZA EXISTS!</h5>
                 
-                <?php endif; ?>
+            <?php endif ; ?>  
         </div>
 
 
